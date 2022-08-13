81552eabf833b4c1d3c7e69df47e10b8ef2fb193
lipikasarkar29 <75445677+lipikasarkar29@users.noreply.github.com>
2022-08-13 16:27:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box;
+function setup(){
+  createCanvas(600,600)
+  
+}
+
+function draw(){
+  background("pink");
+  /*if (keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+3
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.x=box.x-2
+  }*/
+  
+}
